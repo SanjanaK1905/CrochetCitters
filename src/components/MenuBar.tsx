a0a845fc4974b89cb7749ae5eb9e2b3a5dd3af49
Sanjana Karanth <sanjanak@Sanjanas-MacBook-Pro.local>
2025-08-04 22:11:47 -0400

    removed useState.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import{Link} from 'react-router-dom'
 import './MenuBar.css'
 
 function MenuBar() {
-  /*const [count, setCount] = useState(0)*/
 
   return (
     <div className="navbar_container">
